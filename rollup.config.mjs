fa29b502236b0f6820429edc302b9a16003da594
khuyen-le <lenhakhuyen@gmail.com>
2026-06-18 09:28:38 -0400

    update rollup file to resolve build issues

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,3 +1,14 @@
+import resolve from "@rollup/plugin-node-resolve";
 import { makeRollupConfig } from "@jspsych/config/rollup";
 
-export default makeRollupConfig("jsPsychStorycollection");
+export default makeRollupConfig("jsPsychStorycollection").map((config) => {
+  return {
+    ...config,
+    plugins: [
+     ...config.plugins,
+      resolve({
+        extensions: [".js", ".jsx", ".ts"]
+      }),
+    ]
+  };
+});
